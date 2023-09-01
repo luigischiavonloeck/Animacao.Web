b5d2b7bc01058bc95ac437fdf782b6dd30b046ec
LuigiLoeck <luigisloeck@gmail.com>
2023-08-31 23:10:35 -0300

    some anim

--- Projeto Game2d/script.js
+++ Projeto Game2d/script.js
@@ -51,13 +51,24 @@ imgUrls.forEach(url => {
     }
   }
 })
-// var loadSprites = function (url) {
-//   var img = new Image()
-//   img.src = url
-//   img.onload = function () {
-//     loaded++
-//     if (loaded === imgUrls.length) {
-//       console.log('loaded')
-//     }
-//   }
-// }
+
+var textWrapper = document.querySelector('.ml6 .letters')
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g,"<span class='letter'>$&</span>")
+
+textWrapper.addEventListener('mousedown', function () {
+  document.getElementById("play").style
+})
+
+textWrapper.addEventListener('mouseover', function () {
+  letterjump.play()
+})
+
+const letterjump = anime.timeline({ loop: false }).add({
+  targets: '.ml6 .letter',
+  translateY: [0, '-0.5em', 0],
+  translateZ: 0,
+  duration: 550,
+  easing: 'cubicBezier(0.25, 1, 0.5, 1)',
+  delay: (el, i) => (30 * i)-30
+})
+letterjump.pause()
