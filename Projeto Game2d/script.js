38a8581e4a3597d46a9f05d0320af01e33e10ea9
LuigiLoeck <luigisloeck@gmail.com>
2023-08-31 21:08:52 -0300

    load asset fix

--- Projeto Game2d/script.js
+++ Projeto Game2d/script.js
@@ -4,6 +4,7 @@ var ctSquare = document.getElementById('ctArea')
 var trSquare = document.getElementById('trArea')
 var ctInput = ctSquare.querySelector('input')
 var trInput = trSquare.querySelector('input')
+var imgUrls = []
 
 ctSquare.addEventListener('click', function () {
   ctInput.value = true
@@ -15,3 +16,48 @@ trSquare.addEventListener('click', function () {
   trInput.value = true
 })
 
+for (let index = 0; index < 8; index++) {
+  imgUrls.push('./sprites/CT/png/1/idle/1_police_Idle_00' + index + '.png')
+}
+for (let index = 0; index < 8; index++) {
+  imgUrls.push('./sprites/TR/png/1/idle/1_terrorist_1_Idle_00' + index + '.png')
+}
+for (let index = 0; index < 6; index++) {
+  imgUrls.push(
+    './sprites/CT/png/1/attack/1_police_attack_Attack_000_00' + index + '.png'
+  )
+}
+for (let index = 0; index < 4; index++) {
+  imgUrls.push(
+    './sprites/TR/png/1/Attack2/1_terrorist_1_Attack2_00' + index + '.png'
+  )
+}
+for (let index = 0; index < 6; index++) {
+  imgUrls.push('./sprites/TR/png/1/run/1_terrorist_1_Run_00' + index + '.png')
+}
+for (let index = 0; index < 6; index++) {
+  imgUrls.push('./sprites/CT/png/1/run/1_police_Run_00' + index + '.png')
+}
+
+// load sprites images
+let loaded = 0
+imgUrls.forEach(url => {
+  var img = new Image()
+  img.src = url
+  img.onload = function () {
+    loaded++
+    if (loaded === imgUrls.length) {
+      console.log('loaded')
+    }
+  }
+})
+// var loadSprites = function (url) {
+//   var img = new Image()
+//   img.src = url
+//   img.onload = function () {
+//     loaded++
+//     if (loaded === imgUrls.length) {
+//       console.log('loaded')
+//     }
+//   }
+// }
